26e5e7c6fa830f49fa62ef21db9d836672e67d7d
xiaobin0630 <wangxiaobin_hfut@163.com>
2025-07-14 10:53:59 +0800

    修改环境变量

--- launch/multi_robots_avoidance.launch.py
+++ launch/multi_robots_avoidance.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
     collision_radius_check_threshold = 10.0
     try:
         if 'COLLISION_RADIUS_CHECK_THRESHOLD' in os.environ:
-            collision_radius_check_threshold = os.environ.get('COLLISION_RADIUS_CHECK_THRESHOLD')
+            collision_radius_check_threshold = float(os.environ.get('COLLISION_RADIUS_CHECK_THRESHOLD'))
             print(f'get COLLISION_RADIUS_CHECK_THRESHOLD value {collision_radius_check_threshold} from os.environment.')
         else:
             print('using default COLLISION_RADIUS_CHECK_THRESHOLD value 10.0')
@@ -56,7 +56,7 @@ def generate_launch_description():
     global_pose_filter_threshold = 2.82
     try:
         if 'GLOBAL_POSE_FILTER_THRESHOLD' in os.environ:
-            global_pose_filter_threshold = os.environ.get('GLOBAL_POSE_FILTER_THRESHOLD')
+            global_pose_filter_threshold = float(os.environ.get('GLOBAL_POSE_FILTER_THRESHOLD'))
             print(f'get GLOBAL_POSE_FILTER_THRESHOLD value {global_pose_filter_threshold} from os.environment.')
         else:
             print('using default GLOBAL_POSE_FILTER_THRESHOLD value 2.82')
